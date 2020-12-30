d0f5506e4869145f59835a1c5295b850daa3af52
Sungbum Lee <leesb647@gmail.com>
2020-12-30 16:32:49 +0900

    added genre option

--- hello.py
+++ hello.py
@@ -2,9 +2,19 @@ def error():
 	print("\nError:")
 	print("다시 입력해 주세요.")
 
+def get_entire_genres(data):
+	total = []
+	for name in data:
+		for movie_genres in data[name][0]:
+			if movie_genres not in total:
+				total.append(movie_genres)
+	return total
+
 def execute():
-	data = {}
-	favor_genre = []
+	data = {"명량": [["사극","액션"]]}
+	preference = {'장르': [], '시작점': 0, '종료점': 0, '감독': []}
+	entire_genres = get_entire_genres(data)					# 전체 장르 변수.
+
 	while True:
 		print("")
 		print("="*60)
@@ -21,24 +31,22 @@ def execute():
 		if opt == 0:
 			break
 		elif opt == 1:
-			# 장르 메뉴를 보여주기 위한 리스트 만들기 
-			genres = []
-			for name in data:
-				genre = data[name][0]
-				if genre not in genres:
-					genres.append(genre)
-			
 			# 장르별 메뉴 보여주기
 			print("")
 			print("="*60)
-			for i in range(1, len(genres) + 1):
+			for i in range(len(entire_genres)):
 				if i % 5 == 0: 
 					print("")
-				string = (i+1) + ". " + genres         #ex) 1. 로맨스
+				string = str((i+1)) + ". " + entire_genres[i]         #ex) 1. 로맨스
 				print("{:^10}".format(string), end = '')
-			print("{:^10}".format("0. 나가기"))
+			print("{:^10}\n".format("0. 나가기"))
+
 			print("="*60)
 			print("")
+			print("현재 선택된 장르:")
+			for genre in preference['장르']:
+				print(genre, end=' ')
+			print("\n")
 
 			try:
 				idx = int(input("장르를 선택해 주세요: "))
@@ -46,11 +54,11 @@ def execute():
 				error()
 				continue
 
-			if idx >= 0 and idx <= len(genres):
-				if genres[idx - 1] not in favor_genre:
-					favor_genre.append(genres[idx - 1])
+			if idx >= 0 and idx <= len(entire_genres):
+				if entire_genres[idx - 1] not in preference['장르']:
+					preference['장르'].append(entire_genres[idx - 1])
 				else:
-					favor_genre.remove(genres[idx - 1])
+					preference['장르'].remove(entire_genres[idx - 1])
 			else:
 				error()
 				continue
@@ -60,15 +68,30 @@ def execute():
 		elif opt == 2:
 			# 기간 함수
 			pass
+
 		elif opt == 3:
 			# 감독 함수
 			pass
+
 		elif opt == 4:
 			# 배우 함수
 			pass
+
 		else:
 			error()
 			continue
+
+		# 현재 선택된 옵션 출력
+		print("")
+		print("="*20 + "{:^20}".format("현재 선택된 옵션") + "="*20)
+		for key in preference:
+			print(key, " : ", end='')
+			if type(preference[key]) != type(list()):
+				print("{:^5}".format(preference[key]), end='')
+			else:
+				for value in preference[key]:
+					print("{:^5}".format(value),end='')
+			print("")
 
 
 
