c48fe972be8ea76759835f5957b3badccfde94c5
Sungbum Lee <leesb647@gmail.com>
2020-12-30 17:35:52 +0900

    added print_selected_option function and term option

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 def error():
 	print("\nError:")
 	print("다시 입력해 주세요.")
@@ -10,9 +12,18 @@ def get_entire_genres(data):
 				total.append(movie_genres)
 	return total
 
+def print_selected_option(opt, pref):
+	print("")
+	print("현재 선택된 %s:" %opt,end=' ')
+	for value in pref[opt]:
+		print(value, end=' ')
+	print("\n")
+
 def execute():
 	data = {"명량": [["사극","액션"]]}
-	preference = {'장르': [], '시작점': 0, '종료점': 0, '감독': []}
+	today = date.today()
+	opt_list = ['장르', '기간', '감독']
+	preference = {'장르': [], '기간': [today.year, today.year], '감독': []}
 	entire_genres = get_entire_genres(data)					# 전체 장르 변수.
 
 	while True:
@@ -30,6 +41,7 @@ def execute():
 
 		if opt == 0:
 			break
+
 		elif opt == 1:
 			# 장르별 메뉴 보여주기
 			print("")
@@ -40,13 +52,10 @@ def execute():
 				string = str((i+1)) + ". " + entire_genres[i]         #ex) 1. 로맨스
 				print("{:^10}".format(string), end = '')
 			print("{:^10}\n".format("0. 나가기"))
-
 			print("="*60)
-			print("")
-			print("현재 선택된 장르:")
-			for genre in preference['장르']:
-				print(genre, end=' ')
-			print("\n")
+
+			# 현재 선택된 장르 보여주기
+			print_selected_option(opt_list[opt-1], preference)
 
 			try:
 				idx = int(input("장르를 선택해 주세요: "))
@@ -63,18 +72,28 @@ def execute():
 				error()
 				continue
 
-			# 장르 함수
-			pass
 		elif opt == 2:
-			# 기간 함수
-			pass
+			try:
+				start_point = int(input("시작점을 입력하세요: "))
+				end_point = int(input("종료점을 입력하세요: "))
+			except:
+				error()
+				continue
+
+			if end_point < start_point:
+				end_point = start_point
+
+			preference[opt_list[opt-1]][0] = start_point
+			preference[opt_list[opt-1]][1] = end_point
+
+			print_selected_option(opt_list[opt-1], preference)
 
 		elif opt == 3:
 			# 감독 함수
 			pass
 
 		elif opt == 4:
-			# 배우 함수
+			# 영화추천 함수
 			pass
 
 		else:
@@ -83,17 +102,9 @@ def execute():
 
 		# 현재 선택된 옵션 출력
 		print("")
-		print("="*20 + "{:^20}".format("현재 선택된 옵션") + "="*20)
+		print("="*20 + "{:^20}".format("현재 선택된 모든 옵션") + "="*20)
 		for key in preference:
-			print(key, " : ", end='')
-			if type(preference[key]) != type(list()):
-				print("{:^5}".format(preference[key]), end='')
-			else:
-				for value in preference[key]:
-					print("{:^5}".format(value),end='')
-			print("")
-
-
+			print_selected_option(key, preference)
 
 if __name__ == "__main__":
 	execute()
