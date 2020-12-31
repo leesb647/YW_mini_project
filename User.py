2bd7c5d057666082b699f3cb5c90e5a4c1eaac90
Leok90 <leok8907@gmail.com>
2020-12-31 20:16:17 +0900

    practicing dictionary in class

--- User.py
+++ User.py
@@ -32,14 +32,32 @@ class User:
 
     # 영화는 동일한 영화일 경우 별점 갱신 처리하고, 장르와 감독 별점 리스트는 평균값을 구해서 업데이트
     def evaluate_movie(self, movie_name, genre_name, director_name, score):
-        movie_scores[movie_name] = self.movie[movie_name]
+        self.movie_scores[movie_name] = self.movie[movie_name]
 
         genre_scores = self.genre[genre_name]
-        avg_genre_score[genre_name] = sum(genre_scores) / len(genre_scores)
+        self.avg_genre_score[genre_name] = sum(genre_scores) / len(genre_scores)
 
         director_scores = self.director[director_name]
-        avg_director_score[director_name] = sum(director_scores) / len(director_scores)
+        self.avg_director_score[director_name] = sum(director_scores) / len(director_scores)
 
     def change_pwd(self):
         pass
 
+user_list = []
+user_list_id = []
+id = input("ID를 입력하세요 : ")
+pwd = input("비밀번호를 입력하세요 : ")
+
+for i in range(len(user_list)):
+    if id not in user_list_id:
+        user = User(id,pwd)
+        user_list.append(user)
+        user_list_id.append(id)
+
+for idx in range(len(user_list)):
+	if user_list[idx].id == id
+        user_list[idx].add_new_data()
+        user_list[idx].report_score()
+        user_list[idx].evaluate_movie()
+
+user.add_new_data('명량','사극','홍길동')
