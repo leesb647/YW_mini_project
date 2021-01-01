06e9cebeb471ede98c8ac7a0b8cec78be2e2d4ab
Leok90 <leok8907@gmail.com>
2021-01-01 18:27:18 +0900

    added show_all_users func

--- Administrator.py
+++ Administrator.py
@@ -1,5 +1,6 @@
+import math
+
 class Administrator:
-	opt_list = ['나가기', '데이터 관리', '공지 보내기', '유저 상황', '로그아웃']
 	def __init__(self, id, pwd, name):
 		self.id = id
 		self.pwd = pwd
@@ -11,5 +12,32 @@ class Administrator:
 	def send_messege(self):
 		pass
 
-	def show_all_users(self):
-		pass
+	def show_all_users(self, user_list):
+		# user_list = {'1': '영희', '2': '철수', '3': 'ㄱ', '4': 'ㄴ', '5': 'ㄷ'
+		# 	, '6': 'ㄹ', '7': 'ㅁ', '8': 'ㅂ'}
+		user_keys = list(user_list.keys())
+		page = 1
+		end_page = math.ceil(len(user_list) / 5)
+
+		while True:
+			print("\n{0:-^30}".format("유저 정보"))
+			if page == end_page:
+				for i in range(5 * (end_page - 1), len(user_list)):
+					print(user_keys[i], user_list[user_keys[i]])
+			else:
+				for i in range(5 * (page - 1), 5 * page):
+					print(user_keys[i], user_list[user_keys[i]])
+			print("-" * 33)
+			select = int(input("메뉴를 선택하세요( 0.나가기 / 1.이전 / 2.다음 )  : "))
+			if select == 0:
+				break
+			elif select == 1:
+				if page == 1:
+					pass
+				else:
+					page -= 1
+			elif select == 2:
+				if page == end_page:
+					pass
+				else:
+					page += 1
