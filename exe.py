838ce2b5e00ed9a8e8e82209985b71e1dc5e305f
Leok90 <leok8907@gmail.com>
2021-01-02 12:57:12 +0900

    adding sys

--- exe.py
+++ exe.py
@@ -2,6 +2,17 @@ import WebSite
 import sys
 import os
 
+args = sys.argv[1:]
+if args[0] == '-h':
+	print("help list")
+elif args[0] == '-r':
+	f = open("readme.txt", 'r', 'utf-8')
+	while True:
+		line = f.readline()
+		if not line: break
+		print(line)
+	f.close()
+
 if __name__ == "__main__":
 	#argv = sys.argv
 	#args_length = len(argv)
